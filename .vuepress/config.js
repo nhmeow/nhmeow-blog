de90484eef5bc9596ee71d9ef8473a5496562130
Barren Cat <cat@meowy.org>
2020-08-13 06:41:12 +0800

    feat: enable blog plugin

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,29 @@
-
 module.exports = {
     title: 'Barren Cat',
-    description: 'Barren Cat Blog'
+    description: 'Barren Cat Blog',
+    plugins: [
+        [
+            '@vuepress/blog',
+            {
+                directories: [
+                    {
+                        id: 'post',
+                        dirname: 'posts',
+                        path: '/',
+                    },
+
+                ],
+                frontmatters: [
+                    {
+                        id: "tag",
+                        keys: ['tag', 'tags', 'category'],
+                        path: '/categories/',
+                    },
+                ],
+                globalPagination: {
+                    lengthPerPage: 8,
+                },
+            },
+        ],
+    ],
 }
