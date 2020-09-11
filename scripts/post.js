e6fe38859944e859563df4cbec0324a937701fc6
Barren Cat <cat@meowy.org>
2020-09-11 20:57:25 +0800

    feat: add new post script

--- scripts/post.js
+++ scripts/post.js
@@ -1,19 +1,50 @@
-const {writeStream} = require('fs')
-const {moment} = require('moment')
+const readline = require('readline')
+const fs = require('fs')
+const moment = require('moment')
 
-const [, , name, title] = process.argv
+function read(query) {
+    return new Promise((resolve) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
 
-const date = moment().format('YYYY-MM-DD')
+        rl.question(query, (answer) => {
+            rl.close()
+            resolve(answer)
+        })
+    })
+}
 
-const filename = `${__dirname}/../posts/${date}-${name}.md`
-
-const template = `---
+function newPost(name, title, locale) {
+    name = name || 'NewPost'
+    title = title || name
+    locale = locale || ''
+    const date = moment().format('YYYY-MM-DD')
+    const localeDir = locale === '' ? '/' : `/${locale}/`
+    const localeTag = locale === '' ? '' : `_${locale}`
+    const filename = `${__dirname}/..${localeDir}posts/${date}-${name.replace(' ', '-')}.md`
+    const template = `---
 title: ${title}
 date: ${date}
-category: 
+category${localeTag}: 
 ---
 
-## ${title}
+> TODO: Under construction
 `
+    if (fs.existsSync(filename)) {
+        console.log(`file name '${filename}' is exists`)
+        return false
+    }
+    fs.createWriteStream(filename).write(template);
+    return true
+}
 
-writeStream(filename).write(template)
+read('Post Name: ').then((name) => {
+    read('Post Title (Optional): ').then((title) => {
+        if (newPost(name, title) && newPost(name, title, 'zh'))
+            console.log('Okay, well done.')
+        else
+            console.log('Oops, something went wrong.')
+    })
+})
