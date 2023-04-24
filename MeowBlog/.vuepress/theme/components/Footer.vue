e5faea5e6cdbd49d1272834bd940dadec2d80714
Sobte <i@sobte.me>
2023-04-25 02:35:52 +0800

    fix(rss): fixed rss multilingual routing issue

--- MeowBlog/.vuepress/theme/components/Footer.vue
+++ MeowBlog/.vuepress/theme/components/Footer.vue
@@ -1,43 +1,46 @@
 <template>
-  <section class="footer">
-    <div>
-      <router-link to="/about.html#copyright">&copy;{{ new Date().getFullYear() }} Barren Cat</router-link>
-      ,
-      <a href="https://v2.vuepress.vuejs.org">Powered by VuePress2.</a>
-    </div>
-    <div>
-      <a href="/rss.xml">RSS</a>
-    </div>
-  </section>
+    <section class="footer">
+        <div>
+            <router-link to="/about.html#copyright">&copy;{{ new Date().getFullYear() }} Barren Cat</router-link>
+            ,
+            <a href="https://v2.vuepress.vuejs.org">Powered by VuePress2.</a>
+        </div>
+        <div>
+            <a :href="`${routeLocale}rss.xml`">RSS</a>
+        </div>
+    </section>
 </template>
+
+<script setup lang="ts">
+import {useRouteLocale} from "@vuepress/client";
+
+const routeLocale = useRouteLocale();
+</script>
 
 <style scoped>
 .footer {
-  padding: 1rem 0;
-  margin: .5rem 0 0;
-  display: flex;
-  justify-content: space-between;
-  font-size: .8rem;
-  border-top: 1px solid gray;
+    padding: 1rem 0;
+    margin: .5rem 0 0;
+    display: flex;
+    justify-content: space-between;
+    font-size: .8rem;
+    border-top: 1px solid gray;
 }
 
 .mode-dark .footer {
-  border-top: 1px solid white;
+    border-top: 1px solid white;
 }
 
 .footer a {
-  color: gray;
-  text-decoration: none;
+    color: gray;
+    text-decoration: none;
 }
 
 .mode-dark .footer a {
-  color: floralwhite;
+    color: floralwhite;
 }
 
 .footer a:hover {
-  color: orange;
+    color: orange;
 }
 </style>
-
-<script setup lang="ts">
-</script>
